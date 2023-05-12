37b7cbd98fce1e9ca8a6ab28123a6126f5edc140
Femi Solagbade <femi@strangenig.com>
2023-05-12 18:25:33 +0100

    update

--- html/db_conn.php
+++ html/db_conn.php
@@ -9,16 +9,19 @@ $dotenv->load();
 
 // enabling environment variable for php
 
-$servername = $_ENV["MYSQL_IP"]; // input servername
-$username = $_ENV["MYSQL_USER"]; // input username
-$password = $_ENV["MYSQL_PASS"]; //input password
-$dbname = $_ENV["MYSQL_DBNAME"]; // input dbname
+// $servername = $_ENV["MYSQL_IP"]; // input servername
+// $username = $_ENV["MYSQL_USER"]; // input username
+// $password = $_ENV["MYSQL_PASS"]; //input password
+// $dbname = $_ENV["MYSQL_DBNAME"]; // input dbname
 
+$servername = getenv('MYSQL_IP', true) ?: getenv('MYSQL_IP'); // input servername
+$username = getenv('MYSQL_USER', true) ?: getenv('MYSQL_USER'); // input username
+$password  = getenv('MYSQL_PASS', true) ?: getenv('MYSQL_PASS'); //input password
+$dbname = getenv('MYSQL_DBNAME', true) ?: getenv('MYSQL_DBNAME'); // input dbname
 
 // Create connection
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 
 // Check connection
 if ($conn->connect_error) {
